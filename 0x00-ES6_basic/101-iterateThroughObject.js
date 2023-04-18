7007b9ee32ccdc417ea86019d7d8b5e9517cbf44
Oluwatoby8965 <amoniyano1@gmail.com>
2023-04-18 15:25:19 -0400

    iterate object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,7 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return [...reportWithIterator].join(' | ');
+function iterateThroughObject(reportWithIterator) {
+  const employees = Object.values(reportWithIterator);
+  // console.log(employees);
+  return employees.join(' | ');
 }
+
+export default iterateThroughObject;
