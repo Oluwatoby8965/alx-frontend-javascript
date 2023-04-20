d3b59eb9a07145be12d60888f3555fbb9188ea56
Oluwatoby8965 <amoniyano1@gmail.com>
2023-04-19 20:10:24 -0400

    fixing codes

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,9 @@
 function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    } else {
-      return (numerator / denominator);
-    }
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
   }
-  
-  export default divideFunction;
+}
+
+export default divideFunction;
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,13 @@
 function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      queue.push(mathFunction());
-    } catch (error) {
-      queue.push(`Error: ${error.message}`);
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
-  export default guardrail;
-  
+  return queue;
+}
+
+export default guardrail;
